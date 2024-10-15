8ce6d352fbba8730bc307bde1103b77d81f1c71b
Johndhenz <johndhenzdelossantos@gmail.com>
2024-10-15 10:38:57 +0800

    Add files via upload

---
+++ dhenz.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+		float sugarPriced,ricePriceUSD,sardinesPricePound,coffePriceUSD,milkPriceUSD ;
+		float sugarQty,riceQty,sardinesQty,coffeQty,milkQty;
+		float usdToPhp, poundToPhp;
+	
+		cout << "Enter Sugar Quantity";
+		cin >> sugarPriced;
+		cout << "Enter Rice Quantity";
+		cin >> ricePriceUSD;
+		cout << "Enter Sardines Quantity";
+		cin >> sardinesPricePound;
+		cout << "Enter Coffe Quantity";
+		cin >> "Enter Coffe Quantity";
+		cout << "Milk Quantity";
+		cout << "Enter Price of Sugar";
+		cin >> "Price of Sugar";
+		cout << "Enter Price of Rice";
+		cin >> "Price of Rice";
+		cout << "Enter Price of Sardines";
+		cin >> "Price of Sardines";
+		cout << "Enter Price of Coffe";
+		cin >> "Price of Coffe";
+		cout << "Enter Price of Milk";
+		cin >> "Price of MIlk";
+		
+	return 0;
+		
+}
